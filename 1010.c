3125889a8270834c85b9d0f22bbe9190b20b3ea0
grassyau <kj25940858@gmail.com>
2022-10-10 21:39:22 +0800

    10/10

---
+++ 1010.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int line=0,count=0,characters=0,letters=0,digits=0;
+    while ((a=getchar()) != EOF){
+        characters++;
+        if(count==0){
+            printf("%03d:",line+1);
+            count++;
+        }
+        putchar(a);
+        if(a=='\n'){
+            count=0;
+            line++;
+        }
+        if('a'<=a&&a<='z'||'A'<=a&&a<='Z'){
+            letters++;
+        }
+        if('0'<=a&&a<='9'){
+            digits++;
+        }
+    }
+    printf("%d lines, %d characters, %d letters, %d digits",line,characters,letters,digits);
+}
